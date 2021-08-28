8f77dec322c393e3ae90f6725ae5027af3eac231
jorgesalhani <jorge.salhani@usp.br>
2021-08-28 14:06:10 -0300

    add function to store maze into heap memory

--- RunCodes/Exercicio01/ex01.c
+++ RunCodes/Exercicio01/ex01.c
@@ -29,6 +29,41 @@ void read_maze_dimensions(FILE* maze_file, int* M, int* N, int* init_x, int* ini
     *init_y = atoi(init_position_line+2);
 }
 
+char** store_maze_in_matrix(FILE* maze_file, int* M, int* N) {
+    
+    char** maze = NULL;
+    maze = malloc(sizeof(char*)*(*M));
+    char ch;
+    for (int i = 0; i < *M; i++) {
+        maze[i] = malloc(sizeof(char)*(*N));
+        for (int j = 0; j < *N; j++) {
+            ch = fgetc(maze_file);
+            maze[i][j] = ch;
+        }
+        ch = fgetc(maze_file);
+    }
+
+    return maze;
+
+}
+
+void print_maze(char*** e_maze, int* M, int* N) {
+    char** maze = *e_maze;
+    for (int i = 0; i < *M; i++) {
+        for (int j = 0; j < *N; j++) {
+            printf("%c", maze[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void free_maze(char** maze, int* M) {
+    for (int i = 0; i < *M; i++) {
+        free(maze[i]);
+    }
+    free(maze);
+}
+
 int main(void) {
 
     FILE* maze_file = read_labyrinth_from_file();
@@ -36,8 +71,13 @@ int main(void) {
     int M, N, init_x, init_y;
     read_maze_dimensions(maze_file, &M, &N, &init_x, &init_y);
 
+    char** maze = NULL;
+    maze = store_maze_in_matrix(maze_file, &M, &N);
+
+    print_maze(&maze, &M, &N);
 
 
+    free_maze(maze, &M);
     fclose(maze_file);
 
     return 0;
