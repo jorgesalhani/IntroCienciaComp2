bfb2ff78e0d0a75f5a960f70c0908019bc3ef18c
jorgesalhani <jorge.salhani@usp.br>
2021-09-06 08:32:08 -0300

    Add function to treat option 2. Still TODO

--- RunCodes/Trabalho01/t01.c
+++ RunCodes/Trabalho01/t01.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int get_selected_option() {
+int get_selected_option(void) {
     int selected_option;
     scanf("%d ", &selected_option);
 
     return selected_option;
 }
 
-FILE* read_mine_from_file() {
+FILE* read_mine_from_file(void) {
     char mine_filename[100];
     scanf("%s ", mine_filename);
 
@@ -73,6 +73,48 @@ void print_mine_map(char*** pointer_to_mine_map, int* M, int* N) {
     }
 }
 
+void fill_mine_map_with_hints(char*** pointer_to_mine_map, int* M, int* N, int* cursor_M, int* cursor_N) {
+    char** mine_map = *pointer_to_mine_map;
+    int cursor_x = *cursor_M;
+    int cursor_y = *cursor_N;
+
+    if ((cursor_x * cursor_y) == (*M)*(*N)) {
+        return;
+    }
+
+    // printf("%c", *(**pointer_to_mine_map + count));
+    // printf("%c", **pointer_to_mine_map + count);
+    // printf("%d ", *counter);
+    printf("%d %d\n", cursor_x, cursor_y);
+
+    cursor_x++;
+    cursor_y++;
+
+    cursor_x = cursor_x%(*M + 1);
+    cursor_y = cursor_y%(*N + 1);
+
+    *cursor_M = cursor_x;
+    *cursor_N = cursor_y;
+    fill_mine_map_with_hints(pointer_to_mine_map, M, N, cursor_M, cursor_N);
+}
+
+void print_mine_map_with_hints(char*** pointer_to_mine_map, int* M, int* N) {
+    int cursor_M = 0;
+    int cursor_N = 0;
+
+    fill_mine_map_with_hints(pointer_to_mine_map, M, N, &cursor_M, &cursor_N);
+
+    // print_mine_map(pointer_to_mine_map, M, N);
+}
+
+void option_one(char*** pointer_to_mine_map, int* M, int* N, int* selected_option) {
+    if (*selected_option == 1) print_mine_map(pointer_to_mine_map, M, N);
+}
+
+void option_two(char*** pointer_to_mine_map, int* M, int* N, int* selected_option) {
+    if (*selected_option == 2) print_mine_map_with_hints(pointer_to_mine_map, M, N);
+}
+
 int main(void) {
 
     int selected_option = get_selected_option();
@@ -85,7 +127,9 @@ int main(void) {
     N = 0;
 
     mine_map = store_mine_map_from_file(mine_file, &M, &N);
-    print_mine_map(&mine_map, &M, &N);
+
+    option_one(&mine_map, &M, &N, &selected_option);
+    option_two(&mine_map, &M, &N, &selected_option);
 
     free_mine_map(mine_map, &M);
     fclose(mine_file);
