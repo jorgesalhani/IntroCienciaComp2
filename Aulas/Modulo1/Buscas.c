3fc77adf94caff22d6b2cfd7e808480ac335b3cb
jorgesalhani <jorge.salhani@usp.br>
2021-09-16 07:53:20 -0300

    Busca binaria com contagem e insertion sort

--- Aulas/Modulo1/Buscas.c
+++ Aulas/Modulo1/Buscas.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define FAIL -1
+#define FAIL -404
 
 /*
 aloca_e_preenche_vetor
@@ -66,7 +66,7 @@ int busca_sequencial(int* vet, int n, int chave) {
 }
 
 /*
-Busca binaria
+Busca binaria iterativa
 Assume que o vetor esteja ordenado
 @param v (int*) vetor a realizar a busca
 @param ini (int) posicao inicial do vetor
@@ -74,7 +74,7 @@ Assume que o vetor esteja ordenado
 @param chave (int) valor aencontrar
 @return (int) posicao da chave no vetor ou FAIL caso busca sem sucesso
 */
-int busca_binaria(int* v, int ini, int fim, int chave){
+int busca_binaria_iterativa(int* v, int ini, int fim, int chave){
     int c;
     // operacoes:
     // c - comparacao
@@ -101,12 +101,114 @@ int busca_binaria(int* v, int ini, int fim, int chave){
     return FAIL;
 }
 
-int busca_sequencial_recursive(int* v, int chave, int N) {
+/*
+Busca sequencial recursiva
+@param v (int*):    vetor de inteiros para realizar a busca
+@param chave (int): chave a ser buscada
+@param N (int):     tamanho do vetor
+@param i (int):     posicao a ser verificada
+@return (int):      posicao em que a chave foi encontrada, ou FAIL caso contrario
+*/
+int busca_sequencial_recursiva(int* v, int chave, int N, int i) {
     
-    // 1. Caso base (criterio de parada)
-    // 2. Passe recursivo
+    // Contagem:
+    // Comparacoes - c
+    // chamada de funcao - b
+    // aritmeticas - a
+
+    if (i >= N) return FAIL;                                // c
+    if (v[i] == chave) return i;                            // c
+
+    return busca_sequencial_recursiva(v, chave, N, i+1);    // a + b
+                                                            // -----
+                                                            // (2c + a + b)*N
+
+                                                            // f(0) = c
+                                                            // f(N) = (2c + a + b) + f(N-1) <- funcao recorrencia
+
+                                                            // f(K) = (2c + a + b)*K + f(N-K)
+                                                            // Quando N - K = 0, acaba => N = K
+                                                            // f(N) = (2c + a + b)*N + f(0)     <- forma fechada
+                                                            // f(N) = (2c + a + b)*N + c
+}
+
+/*
+Busca binaria recursiva
+Assume que o vetor esteja ordenado
+@param v (int*) vetor a realizar a busca
+@param ini (int) posicao inicial do vetor
+@param fim (int) posicao final do vetor a ser buscada
+@param chave (int) valor aencontrar
+@return (int) posicao da chave no vetor ou FAIL caso busca sem sucesso
+*/
+int busca_binaria(int* v, int chave, int ini, int fim) {
     
-    // if (v[i] == chave) return i;
+    // Contagem:
+    // Comparacoes - c
+    // chamada de funcao - b
+    // aritmeticas - a
+
+
+    // Caso base 1: parada
+    if (ini > fim) return FAIL; // Criterio de parada   // c
+
+    int c = (int)((ini + fim) / 2.0);                   // 2a
+
+    // Caso base 2: chave encontrada
+    if (v[c] == chave) return c;                        // c
+
+    if (chave < v[c]) {                                 // c
+        return busca_binaria(v, chave, ini, c-1);       // a + b
+    }
+
+    if (chave > v[c]) {                                 // c
+        return busca_binaria(v, chave, c+1, fim);       // a + b
+                                                        // -----
+                                                        // c + 2a + c + c + c + a + b
+                                                        // 4c + 3a + b
+                                                        // f(0) = c
+                                                        // f(N) = (4c + 3a + b) + f((N-1)/2)   <- funcaorecorrencia (pior caso)
+                                                        // f(N) = (4c + 3a + b) + f(N/2) ~
+                                                        // f(N) = (4c + 3a + b)*2 + f(N/(2*2)) ~
+
+                                                        // f(K) = (4c + 3a + b)K + f(N/2^K)
+                                                        // Quando N/2^K = 1, acaba  => N = 2^K
+                                                        //                          => K = log2(N)
+                                                        // f(N) = (4c + 3a + b) log2(N) + f(1)
+                                                        // Com f(1) = 3a + b + 5c   <- 1 exec + f(0)
+    }
+}
+
+/*
+Insertion sort
+    Metodo de insercao na posicao correta
+    Metodo de ordenacao inplace (altera o vetor)
+@param v (int*):    vetor a ser ordenado
+@param n (int):     tamanho do vetor
+*/
+void insertion_sort(int* v, int N) {
+
+    // vetor
+    // [ LO | LD ]
+    // [ 8 | 7 0 1 ]
+    // [ 7 8 | 0 1 ]
+    // [ 0 7 8 | 1 ]
+    // [ 0 1 7 8 | ]
+
+    int i = 1;
+
+    while (i < N) {
+        int el = v[i]; // fixa elemento a ser comparado com anteriores
+        int j = i - 1; // indice para percorrer elementos anteriores
+
+        while (j >= 0 && el < v[j]) {
+            v[j + 1] = v[j];    // Movimenta valor para frente
+            j--;                // indice retorna uma posicao
+        }
+        v[i-1] = el; // Encontrei a posicao correta
+
+        i++;
+    }
 }
 
 int main (int argc, char* argv[]) {
@@ -131,10 +233,22 @@ int main (int argc, char* argv[]) {
 
     printf("Vetor de tamanho: %d, maximo = %d\n", n, max);
 
-    int* v = NULL;
-    v = aloca_e_preenche_vetor(n, max, ord, verb);
+    srand(0);
+    int* v1 = NULL;
+    v1 = aloca_e_preenche_vetor(n, max, ord, verb);
+    
+    srand(0);
+    int* v2 = NULL;
+    v2 = aloca_e_preenche_vetor(n, max, 0, verb);
 
-    if (v == NULL) {
+    clock_t t1, t2;
+
+    t1 = clock();
+    insertion_sort(v2, n);
+    t2 = clock();
+    double time_is = (t2-t1)/(double)CLOCKS_PER_SEC;
+
+    if (v1 == NULL) {
         printf("Nao foi possivel alocar memoria\n");
         return 3;
     }
@@ -142,26 +256,35 @@ int main (int argc, char* argv[]) {
     int chave;
     printf("Chave: "); scanf("%d", &chave);
 
-    clock_t t1, t2;
     t1 = clock();
-    int ps = busca_sequencial(v, n, chave);
+    int ps = busca_sequencial(v1, n, chave);
     t2 = clock();
     double time_s = (t2-t1)/(double)CLOCKS_PER_SEC;
 
     t1 = clock();
-    int pb = busca_binaria(v, 0, n-1, chave);
+    int pb = busca_binaria_iterativa(v1, 0, n-1, chave);
     t2 = clock();
     double time_b = (t2-t1)/(double)CLOCKS_PER_SEC;
 
+    t1 = clock();
+    int pbr = busca_sequencial_recursiva(v1, 0, n-1, chave);
+    t2 = clock();
+    double time_sr = (t2-t1)/(double)CLOCKS_PER_SEC;
+
     if (ps >= 0) printf("Sequencial: chave encontrada em %d\n", ps);
-    else if (ps == FAIL) printf("Sequencia: busca sem sucesso\n");
+    else if (ps == FAIL) printf("Sequencial: busca sem sucesso\n");
 
     if (pb >= 0) printf("Binaria: chave encontrada em %d\n", pb);
     else if (pb == FAIL) printf("Binaria: busca sem sucesso\n");
 
+    if (pb >= 0) printf("Sequencial recursiva: chave encontrada em %d\n", pbr);
+    else if (pb == FAIL) printf("Sequencial recursiva: busca sem sucesso\n");
+
     printf("Tempo execucao sequencial: %lf seg.\n", time_s);
     printf("Tempo execucao binaria: %lf seg.\n", time_b);
+    printf("Tempo execucao sequencial recursiva: %lf seg.\n", time_sr);
+    printf("Tempo execucao insertion sort: %lf seg.\n", time_is);
 
-    free(v);
+    free(v1);
     return 0;
 }
