51bccbd9164b69e6da4882d6f8535a0febf99e36
jorgesalhani <jorge.salhani@usp.br>
2021-10-01 00:10:36 -0300

    Read wav file now working

--- RunCodes/Trabalho02/t02.c
+++ RunCodes/Trabalho02/t02.c
@@ -10,9 +10,10 @@ Titulo:     Trabalho 02: Tratamento de audio
 #include <complex.h>
 
 FILE* read_wav_filename(void) {
-    char wav_filename[100];
-    scanf("%s ", wav_filename);
+    // char wav_filename[100];
+    // scanf("%s ", wav_filename);
 
+    char wav_filename[50] = "cat.wav";   
     FILE* wav_file = fopen(wav_filename, "rb");
     if (wav_file == NULL) {
         perror("Error opening file!\n");
@@ -23,45 +24,41 @@ FILE* read_wav_filename(void) {
 }
 
 void read_compression_coeff_input(int* compression_coeff) {
-    scanf("%d ", compression_coeff);
+    // scanf("%d ", compression_coeff);
+    *compression_coeff = 100;
 }
 
-unsigned char* store_wav_binary_content(FILE* wav_file, long int* content_length) {
-    fseek(wav_file, 0, SEEK_END);
-    long int file_size = ftell(wav_file);
-    *content_length = file_size;
+unsigned char* store_wav_binary_content(FILE* fp, int* content_length) {
+    unsigned char buf4[4];
 
-    unsigned char* wav_content = calloc(file_size, sizeof(unsigned char));
+    fseek(fp, 40, SEEK_SET);
+    fread(buf4, sizeof(buf4), 1, fp);
+    int dataSize = buf4[0] | buf4[1]<<8 | buf4[2]<<16 | buf4[3]<<24;
 
-    int i = 1;
-    while (i < file_size) {
-        fread(&wav_content[i], sizeof(unsigned char), 1, wav_file);
-        i++;
+    unsigned char* data = malloc(sizeof(*data) * (dataSize));
+    
+    int i = 0;
+    while (i < dataSize) {
+        fread(&data[i++], sizeof(unsigned char), 1, fp);
     }
-
-    fclose(wav_file);
-
-    return wav_content;
+    fclose(fp);
+    *content_length = dataSize;
+    return data;
 }
 
 
 int main (void) {
-
     FILE* wav_file = read_wav_filename();
 
     int compression_coeff = 0;
     read_compression_coeff_input(&compression_coeff);
 
-    long int content_length = 0;
+    int content_length = 0;
     unsigned char* wav_file_content = NULL;
     wav_file_content = store_wav_binary_content(wav_file, &content_length);
 
-    for(int i = 0; i<10; i++) printf("%u ", wav_file_content[i]);
+    for(int i = 0; i<10; i++) printf("%hhx ", wav_file_content[i]);
     printf("\n");
-    // printf("%ld\n", content_length);
-    // printf("%u\n", wav_file_content);
-
-
 
     free(wav_file_content);
 
