8d470d0e7c7654fe65e000631fb440c2d94c34cc
jorgesalhani <jorge.salhani@usp.br>
2021-10-23 14:45:52 -0300

    Start quadtree Ex02

---
+++ RunCodes/Exercicio02/ex02.c
@@ -0,0 +1,6 @@
+/* HEADER
+Autor:      Jorge Augusto Salgado Salhani --- no USP: 8927418 ---
+Disciplina: SCC0201 Introducao a Ciencia da Computacao - 2o sem 2021
+Titulo:     Exercicio 2: Compressao de imagem com quadtree
+*/
+
