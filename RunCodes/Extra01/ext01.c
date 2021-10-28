c16cf9319defb507df2b15b097253653086d4fcc
jorgesalhani <jorge.salhani@usp.br>
2021-10-27 21:36:19 -0300

    HUTTY: bin tree understanding

--- RunCodes/Extra01/ext01.c
+++ RunCodes/Extra01/ext01.c
@@ -2,80 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 
-struct tree {
-    char info;
-    struct tree *left;
-    struct tree *right;
+struct Tree {
+    int info;
+    struct Tree *up_left;
+    struct Tree *up_right;
+    struct Tree *down_left;
+    struct Tree *down_right;
 };
 
-struct tree *root;
-struct tree *stree(struct tree *root, struct tree *r, char info);
-void print_tree(struct tree *root, int l);
+struct Point {
+    int x;
+    int y;
+};
 
-void main(void) {
-    char s[80] = "acbad\0";
-    root = NULL;
+struct Node {
+    int x0;
+    int y0;
+    int height;
+    int width;
+    struct Point* points;
+};
 
-    int c = 0;
-    do {
-        // printf("Insira uma letra: ");
-        // scanf("%s", s);
-        printf("%c\n", s[c]);
-        if (!root) {
-            root = stree(root, root, *(s+c));
-        } else {
-            stree(root, root, *(s+c));
-        }
-        c++;
-        printf("CURSOR: %p %p %c\n", root->left, root->right, root->info);
-    } while (c < strlen(s));
-
-    print_tree(root, 0);
+struct Point* contains(int x0, int y0, int w, int h, struct Point* points) {
+    struct Point* pts = (struct Point*) malloc(sizeof(struct Point));
+    for (int i = 0; i < )
 }
 
-struct tree *stree(struct tree *root, struct tree *r, char info) {
-    if (!r) {
-        r = (struct tree *) malloc(sizeof(struct tree));
-        if (!r) {
-            printf("Sem memoria\n");
-            exit(0);
-        }
-        r -> left = NULL;
-        r -> right = NULL;
-        r -> info = info;
-
-        if (!root) {
-            return r;
-        }
-
-        if (info != root -> info) {
-            root -> left = r;
-        } else {
-            root -> right = r;
-        }
-        return r;
+void recursive_subdivide(struct Node* node) {
+    if (!node->points) {
+        return;
     }
 
-    if (info != r -> info) {
-        stree(r, r -> left, info);
-    } else {
-        stree(r, r -> right, info);
-    }
-    printf("ROOT: %p %p %c\n", root->left, root->right, root->info);
-    printf("R: %p %p %c\n\n", r->left, r->right, r->info);
+    int w_ = node->width;
+    int h_ = node->height;
 
+    struct Point* p = contains(node->x0, node->y0, w_, h_, node->points);
 }
 
-void print_tree(struct tree *r, int l) {
-    int i;
-    if (!r) return;
-
-    print_tree(r->right, l+1);
-    for (i = 0; i < 1; ++i) {
-        printf("  ");
-    }
-    printf("%c\n", r -> info);
-    print_tree(r -> left, l+1);
-
-    free(r);
+int main(void) {
+    
+    
+    return 0;
 }
