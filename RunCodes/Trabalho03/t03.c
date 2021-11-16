67abf4c7cc01f40eefa3c9c86e739c51f3c068c5
Jorge Salhani <jorge.salhani@qitech.com.r>
2021-11-16 01:10:51 -0300

    Start T3

---
+++ RunCodes/Trabalho03/t03.c
@@ -0,0 +1,7 @@
+/* HEADER
+Autor:      Jorge Augusto Salgado Salhani --- no USP: 8927418 ---
+Disciplina: SCC0201 Introducao a Ciencia da Computacao - 2o sem 2021
+Titulo:     Trabalho 03: Escalonador de processos
+*/
+
+
