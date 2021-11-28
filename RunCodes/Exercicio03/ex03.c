5fb08097938e176e6abea8e1eaf6ef511dd80997
Jorge Salhani <jorge.salhani@qitech.com.r>
2021-11-28 11:09:42 -0300

    Header to ex3

---
+++ RunCodes/Exercicio03/ex03.c
@@ -0,0 +1,14 @@
+/* HEADER
+Autor:      Jorge Augusto Salgado Salhani --- no USP: 8927418 ---
+Disciplina: SCC0201 Introducao a Ciencia da Computacao - 2o sem 2021
+Titulo:     Exercicio 3: Busca Indexada com Lista Encadeada
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+
+    return 0;
+}
