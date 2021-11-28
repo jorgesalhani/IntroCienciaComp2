c558ed6aec42a7f645658eea4fddbf6905b0aabd
Jorge Salhani <jorge.salhani@qitech.com.r>
2021-11-28 11:59:13 -0300

    Start class module 3

--- RunCodes/Exercicio03/ex03.c
+++ RunCodes/Exercicio03/ex03.c
@@ -8,7 +8,50 @@ Titulo:     Exercicio 3: Busca Indexada com Lista Encadeada
 #include <stdio.h>
 #include <stdbool.h>
 
+void read_command(int* end_command) {
+    int command = *end_command;
+    scanf("%d ", &command);
+    *end_command = command;
+}
+
+void read_file() {
+
+}
+
+void create_list() {
+
+}
+
+void read_file_and_create_list(void) {
+    read_file();
+
+    create_list();
+}
+
+void create_update_index_vector() {
+
+}
+
+void search() {
+    
+}
+
+void process_command(int* command) {
+    if (*command == 1) read_file_and_create_list();
+
+    if (*command == 2) create_update_index_vector();
+
+    if (*command == 3) search();
+
+}
+
 int main(void) {
+
+    int command = 0;
+    read_command(&command);
+
+    process_command(&command);
+
 
     return 0;
 }
