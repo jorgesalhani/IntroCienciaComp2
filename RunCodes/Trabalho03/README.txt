5ac74010dc610acb8b0a018a5ed2c35d56cf80d5
Jorge Salhani <jorge.salhani@qitech.com.r>
2021-12-05 15:24:39 -0300

    Restarting Trabalho03

--- RunCodes/Trabalho03/README.txt
+++ RunCodes/Trabalho03/README.txt
@@ -1,3 +1,3 @@
-Arquivo zip gerado em: 16/11/2021 01:08:05 
+Arquivo zip gerado em: 05/12/2021 15:23:16 
 Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
 Exercício: Trabalho 03 - Simulador de Escalonamento de Processos
--- RunCodes/Trabalho03/t03.c
+++ RunCodes/Trabalho03/t03.c
@@ -71,88 +71,16 @@ void made_code_unique_recursively(int*** ptr_process_list, int* N, bool changes_
     }
 }
 
-int** order_by_priority(int*** ptr_process_list, int* N) {
+void select_scheduler_algorithm(int*** ptr_process_list, int* N, int* total_processes) {
     int** process_list = *ptr_process_list;
 
-    int** ordered_list = NULL;
-    ordered_list = (int**)malloc(sizeof(int*)*(*N));
-    
-    for (int i = 0; i < *N; i++) {
-        ordered_list[i] = NULL;
-        ordered_list[i] = (int*)calloc(4, sizeof(int));
-    }
-
-    for (int i = 0; i < *N; i++) {
-        int priority = process_list[i][3];
-        if (ordered_list[priority-1][0] == 0) {
-            for (int j = 0; j < 4; j++) {
-                ordered_list[priority-1][j] = process_list[i][j];
-            }
-        }
-    }
-
-    return ordered_list;
-}
-
-void round_robin(int*** ptr_ordered_list, int* N, int* total_processes, int* process_i, int* total_time) {
-    int** ordered_list = *ptr_ordered_list;
-
-    int time_to_end = ordered_list[*process_i][2];
-    if (time_to_end == 0) {
-        int N_ = *N;
-        N_--;
-        *N = N_;
-    }
-
-    if (*N == 0) return;
-
-    int time_to_init = ordered_list[*process_i][1];
-    if (time_to_init != *total_time) {
-        int process_i_ = *process_i;
-        process_i_ = (*process_i+1)%4;
-        *process_i = process_i_;
-        return round_robin(ptr_ordered_list, N, total_processes, process_i, total_time);
-    } else {
-        ordered_list[*process_i][1]++;
-        ordered_list[*process_i][2]--;
-    }
-
-    bool same_time_process = false;
-    for (int i = *process_i+1; i < *total_processes; i++) {
-        if (ordered_list[i][1] == *total_time) {
-            same_time_process = true;
-            break;
-        }
-    }
-
-    if (!same_time_process) {
-        int total_time_ = *total_time;
-        total_time_++;
-        *total_time = total_time_;
-    }
-
-    int process_i_ = *process_i;
-    process_i_ = (*process_i+1)%4;
-    *process_i = process_i_;
-    
-    round_robin(ptr_ordered_list, N, total_processes, process_i, total_time);
-}
-
-void select_scheduler_algorithm(int*** ptr_process_list, int* N, int* total_processes) {
-    int** ordered_list = NULL;
-    ordered_list = order_by_priority(ptr_process_list, N);
-
-    print_process_list(ordered_list, N);
+    printf("ORIGINAL:\n");
+    print_process_list(process_list, N);
     printf("\n");
 
     int process_i = 0;
     int total_time = 1;
-    round_robin(&ordered_list, N, total_processes, &process_i, &total_time);
 
-    print_process_list(ordered_list, total_processes);
-    printf("\n");
-
-    free_matrix(ordered_list, total_processes);
 }
 
 
